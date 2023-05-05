e42261fe94450f8a3465f285172646017ebe9050
unknown <the.nuyie@gmail.com>
2023-05-05 23:14:05 +0700

    completed code for connecting with postgresql.

--- src/main/java/com/example/springbootsample/SpringBootSampleApplication.java
+++ src/main/java/com/example/springbootsample/SpringBootSampleApplication.java
@@ -2,8 +2,10 @@ package com.example.springbootsample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SpringBootSampleApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/springbootsample/data/repository/PostRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.springbootsample.data.repository;
-
-import com.example.springbootsample.data.entity.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Long> {
-
-}
